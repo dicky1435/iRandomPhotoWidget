dd120e1da9c1f9e48bc5d43397696465b9fdbed1
dicky1435 <45350950+dicky1435@users.noreply.github.com>
2020-12-01 15:29:51 +0800

    Add files via upload

---
+++ iRandomPhotoTimeBanner/iRandomPhotoTimeBanner.swift
@@ -0,0 +1,194 @@
+//
+//  iRandomPhotoTimeBanner.swift
+//  iRandomPhotoTimeBanner
+//
+//  Created by Dicky on 1/12/2020.
+//
+import Combine
+import WidgetKit
+import SwiftUI
+import Intents
+//
+//class BatteryModel : ObservableObject {
+//    @Published var level = UIDevice.current.batteryLevel
+//    private var cancellableSet: Set<AnyCancellable> = []
+//
+//    init () {
+//        UIDevice.current.isBatteryMonitoringEnabled = true
+//        assignLevelPublisher()
+//    }
+//
+//    private func assignLevelPublisher() {
+//        _ = UIDevice.current
+//            .publisher(for: \.batteryLevel)
+//            .assign(to: \.level, on: self)
+//            .store(in: &self.cancellableSet)
+//    }
+//}
+
+struct Provider: IntentTimelineProvider {
+    //@ObservedObject var batteryModel = BatteryModel()
+    
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(),batteryNumber: "loading",batteryState: UIDevice.current.batteryState ,configuration: ConfigurationIntent())
+    }
+    
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        let midnight = Calendar.current.startOfDay(for: Date())
+        let entries = SimpleEntry(date: midnight,batteryNumber: String(format:"%.f%%", UIDevice.current.batteryLevel * 100), batteryState: UIDevice.current.batteryState, configuration: configuration)
+        completion(entries)
+    }
+    
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        let midnight = Calendar.current.startOfDay(for: Date())
+        let nextMidnight = Calendar.current.date(byAdding: .minute, value: 5, to: midnight)!
+        let entries = [SimpleEntry(date: midnight,batteryNumber: String(format:"%.f%%", UIDevice.current.batteryLevel * 100),  batteryState: UIDevice.current.batteryState, configuration: configuration)]
+        let timeline = Timeline(entries: entries, policy: .after(nextMidnight))
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    var date: Date
+    var batteryNumber : String
+    var batteryState : UIDevice.BatteryState
+    let configuration: ConfigurationIntent
+}
+
+struct widgetEntryView : View {
+    var entry: Provider.Entry
+    @State var imageArray = [UIImage(named: "infinity-war"), UIImage(named: "endgame"), UIImage(named: "sun")];
+    @Environment(\.widgetFamily) var family
+    let key = "randomImage"
+    
+    var gradient: Gradient {
+        Gradient(stops:
+                    [Gradient.Stop(color: .clear, location: 0),
+                     Gradient.Stop(color: Color.black.opacity(0.95),
+                                   location: 0.6)])
+    }
+    
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "hh:mm:ss a"
+        return formatter
+    }()
+    
+    func getImageKey(_ index:Int) -> String {
+        return "\(key)\(index)"
+    }
+    
+    func loadImages() -> [UIImage] {
+        let list = UserDefaults(suiteName: "group.dicky.iRandomPhotoWidget")!.array(forKey: key) as? [String] ?? [String]()
+        UserDefaults(suiteName: "group.dicky.iRandomPhotoWidget")!.synchronize()
+        var imageList = [UIImage]()
+        for (index, _) in list.enumerated() {
+            let imageName = getImageKey(index)
+            if let shareUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.dicky.iRandomPhotoWidget") {
+               let imagePath = shareUrl.appendingPathComponent(imageName)
+                if let image = UIImage(contentsOfFile: imagePath.path) {
+                    imageList.append(image)
+                }
+            }
+        }
+        return imageList
+    }
+    
+    var body: some View {
+        ZStack {
+            Image(uiImage: imageArray.randomElement()!!)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .mask(
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .frame(minHeight: 0, maxHeight: .infinity)
+                )
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .frame(minHeight: 0, maxHeight: .infinity)
+
+            VStack {
+                Spacer()
+                LinearGradient(gradient: gradient,
+                               startPoint: .top,
+                               endPoint: .bottom)
+                    .frame(height: 100)
+            }
+            
+           
+            
+            VStack(alignment: .leading) {
+                Spacer()
+                HStack(alignment: .bottom) {
+                    Text(entry.date,style: .timer)
+                        .padding(.all, family == .systemSmall ? 7 : (family == .systemMedium ? 10 : 15))
+                        .font(.headline)
+                        .foregroundColor(.white)
+                    
+                    Text(entry.batteryNumber)
+                        .padding(.all, family == .systemSmall ? 7 : (family == .systemMedium ? 10 : 15))
+                        .font(.headline)
+                        .foregroundColor(.white)
+                }
+                
+            }
+            
+        }
+        .onAppear(perform: {
+            if (loadImages().count != 0) {
+                self.imageArray = loadImages()
+            }
+        })
+        .background(Color.black)
+    }
+    
+}
+
+struct PlaceHolder : View {
+    var entry: Provider.Entry
+    
+    var body: some View {
+        
+        VStack {
+            HStack{
+                Spacer()
+                Text("Time")
+                Spacer()
+            }
+            
+            Spacer()
+            
+            Text("Loading")
+                .padding(.horizontal, 15)
+            
+            Spacer()
+        }
+        .background(Color.black)
+    }
+    
+}
+
+@main
+struct widget: Widget {
+    let kind: String = "widget"
+    
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            widgetEntryView(entry: entry)
+            
+        }
+        .configurationDisplayName("My Widget")
+        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
+        .description("This is an example widget.")
+    }
+}
+
+struct widget_Previews: PreviewProvider {
+    static var previews: some View {
+        widgetEntryView(entry: SimpleEntry(date: Date(),batteryNumber: "loading",batteryState: UIDevice.BatteryState.unknown, configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
---
+++ iRandomPhotoWidget/ContentView.swift
@@ -0,0 +1,421 @@
+//
+//  ContentView.swift
+//  iRandomPhotoWidget
+//
+//  Created by Dicky on 1/12/2020.
+//
+
+import SwiftUI
+import Combine
+import PhotosUI
+import WidgetKit
+
+class BatteryModel : ObservableObject {
+    @Published var level = UIDevice.current.batteryLevel
+    private var cancellableSet: Set<AnyCancellable> = []
+
+    init () {
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        assignLevelPublisher()
+    }
+
+    private func assignLevelPublisher() {
+        UIDevice.current
+            .publisher(for: \.batteryLevel)
+            .assign(to: \.level, on: self)
+            .store(in: &self.cancellableSet)
+    }
+}
+
+struct ContentView: View {
+  
+    @ObservedObject var batteryModel = BatteryModel()
+    @State var goToHome = false
+    @State var animate = false
+    @State var endSplash = false
+    var body: some View {
+
+        ZStack{
+            
+            ZStack {
+                Rectangle()
+                    .fill(Color.init(red: 34/255, green: 30/255, blue: 47/255))
+                    .edgesIgnoringSafeArea(.all)
+                if goToHome{
+                    OnCircularView()
+                }else{
+                    OnBoardScreen()
+                }
+            }
+            .onReceive(NotificationCenter.default.publisher(for: NSNotification.Name("Success")), perform: { _ in
+                withAnimation{self.goToHome = true}
+            })
+            
+            ZStack{
+                Color("bg")
+                
+                Image("snapchatLarge")
+                    .resizable()
+                    .renderingMode(.original)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 85, height: 85)
+                    .scaleEffect(animate ? 20 : 1)
+                    .frame(width: UIScreen.main.bounds.width)
+            }
+            .ignoresSafeArea(.all,edges: .all)
+            .onAppear(perform: animationSpalsh)
+            .opacity(endSplash ? 0 : 1)
+        }
+    }
+    
+    func animationSpalsh() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+            
+            withAnimation(Animation.easeOut(duration:1)){
+                animate.toggle()
+            }
+            
+            withAnimation(Animation.easeOut(duration:1)){
+                endSplash.toggle()
+            }
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct OnCircularView: View  {
+    @State var images : [UIImage] = [UIImage (named: "endgame")!]
+    @State var picker = false
+    @State var alertBox = false
+    @ObservedObject var batteryModel = BatteryModel()
+    let key = "randomImage"
+    
+    func getImageKey(_ index:Int) -> String {
+        return "\(key)\(index)"
+    }
+    
+    func loadImages() -> [UIImage] {
+        let list = UserDefaults(suiteName: "group.dicky.iRandomPhotoWidget")!.array(forKey: key) as? [String] ?? [String]()
+        var imageList = [UIImage]()
+        for (index, _) in list.enumerated() {
+            let imageName = getImageKey(index)
+            if let shareUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.dicky.iRandomPhotoWidget") {
+               let imagePath = shareUrl.appendingPathComponent(imageName)
+                if let image = UIImage(contentsOfFile: imagePath.path) {
+                    imageList.append(image)
+                }
+            }
+        }
+        return imageList
+    }
+    
+    var body: some View {
+        
+        VStack(spacing: 20){
+            Spacer()
+            Spacer()
+            Spacer()
+            
+            GeometryReader { proxy in
+                ScrollView(.horizontal, showsIndicators: false, content: {
+                    HStack() {
+                        ForEach(images.indices, id: \.self) { (index) in
+                            Image(uiImage: images[index])
+                                .resizable()
+                                .scaledToFill()
+                                .overlay(Color.black.opacity(0.4))
+                                .frame(width: proxy.size.width, height: 200)
+                            
+                        }
+                    }
+                })
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+                .padding()
+                .frame(width: proxy.size.width, height: proxy.size.height / 3)
+//                TabView {
+//                }.tabViewStyle(PageTabViewStyle())
+//                .clipShape(RoundedRectangle(cornerRadius: 5))
+//                .tag(images.count)
+//                .padding()
+//                .frame(width: proxy.size.width, height: proxy.size.height / 3)
+            }.onAppear(perform: {
+                images = loadImages()
+            })
+            
+            
+            ZStack() {
+                Circle()
+                    .stroke(Color.white, lineWidth: 3)
+                    .opacity(0.1)
+
+                Circle()
+                    .trim(from: 0, to: CGFloat(batteryModel.level))
+                    .stroke(batteryModel.level > 0.5 ? Color.green : Color.orange, lineWidth: 3)
+                    .rotationEffect(.degrees(-90))
+                    .overlay(Text("\(Int(round(batteryModel.level * 100)))%")
+                                .foregroundColor(Color.white))
+                
+                Circle()
+                    .fill(batteryModel.level > 0.5 ? Color.green : Color.orange)
+                    .frame(width: 15 * 2, height: 15 * 2)
+                    .padding(10)
+                    .offset(y: -128)
+                    .rotationEffect(Angle.degrees(Double(batteryModel.level * 360)))
+                
+            }.padding(20)
+            .frame(height: 300)
+            
+            Spacer()
+            Spacer()
+            Spacer()
+            
+            Button(action: {
+                picker.toggle()
+            }) {
+                HStack {
+                    Image(systemName: "photo.on.rectangle")
+                        .font(.title)
+                    Text("Select Photo")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 20, maxHeight: 25)
+                .padding()
+                .foregroundColor(.white)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(40)
+                .padding(.horizontal, 20)
+            }
+        }.sheet(isPresented: $picker) {
+            ImagePicker(alertBox: $alertBox, images: $images, picker: $picker)
+        }.alert(isPresented: $alertBox) {
+            Alert(title: Text("Photos saved!"), dismissButton: .default(Text("OK")) {
+                alertBox = false
+            })
+        }
+    }
+}
+
+struct OnBoardScreen: View {
+    
+    @State var maxWidth = UIScreen.main.bounds.width - 100
+    @State var offset : CGFloat = 0
+    
+    var body: some View{
+        
+        ZStack{
+            
+            Color(.black)
+                .ignoresSafeArea(.all
+                                 ,edges: .all)
+            
+            VStack {
+                Image(uiImage: UIImage(named: "infinity-war")!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(minHeight: 0, maxHeight: .infinity)
+                    .ignoresSafeArea(.all
+                                     ,edges: .all)
+                
+                // Slider
+                
+                ZStack {
+                    Capsule()
+                        .fill(Color.white.opacity(0.1))
+                    
+                    Text("SWIPE TO START")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding(.leading,30)
+                    
+
+                    HStack{
+                        Capsule()
+                            .fill(Color(.red))
+                            .frame(width: calculateWidth() + 65)
+
+                        Spacer(minLength: 0)
+                    }
+                    
+                    HStack{
+                        ZStack{
+                            Image(systemName: "chevron.right")
+                            
+                            Image(systemName: "chevron.right")
+                                .offset(x:-10)
+                            
+                        }.foregroundColor(.white)
+                        .offset(x:5)
+                        .frame(width: 65, height: 65)
+                        .background(Color(.red))
+                        .clipShape(Circle())
+                        .offset(x:offset)
+                        .gesture(DragGesture().onChanged(onChanged(value:)).onEnded(onEnd(value:)))
+                        
+                        Spacer()
+                    }
+                    
+                    
+                }
+                .frame(width: maxWidth, height: 65)
+                .padding(.bottom)
+            }
+            
+        }
+    }
+    
+    func calculateWidth() -> CGFloat {
+        let percent = offset / maxWidth
+        
+        return percent * maxWidth
+    }
+    
+    func onChanged(value: DragGesture.Value){
+        if value.translation.width > 0 && offset <= maxWidth - 65 {
+            offset = value.translation.width
+        }
+    }
+    
+    func onEnd(value: DragGesture.Value){
+        
+        withAnimation(Animation.easeOut(duration:0.3)) {
+            if offset > 180 {
+                offset = maxWidth - 65
+                
+                DispatchQueue.main.asyncAfter(deadline: .now()) {
+                    NotificationCenter.default.post(
+                        name: NSNotification.Name("Success"), object: nil
+                    )
+                }
+            } else {
+                offset = 0
+            }
+        }
+    }
+}
+
+struct ImagePicker : UIViewControllerRepresentable {
+    func makeCoordinator() -> Coordinator {
+        return ImagePicker.Coordinator(parent1: self)
+    }
+    
+    @Binding var alertBox : Bool
+    @Binding var images : [UIImage]
+    @Binding var picker : Bool
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 0
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject,PHPickerViewControllerDelegate {
+        
+        let key = "randomImage"
+        
+        var parent : ImagePicker
+        var tempImages : [UIImage]
+        
+        init(parent1: ImagePicker) {
+            parent = parent1
+            tempImages = []
+        }
+        
+        func getImageKey(_ index:Int) -> String {
+            return "\(key)\(index)"
+        }
+
+        func saveImages(_ images:[UIImage]) -> Bool{
+            var saveAction = false
+            removeImages()
+            var list = UserDefaults(suiteName: "group.dicky.iRandomPhotoWidget")!.array(forKey: key) as? [String] ?? [String]()
+            list.removeAll()
+            var index = list.count
+            
+            for image in images {
+                let imgKey = getImageKey(index)
+                saveImage(imgKey, image)
+                list.append(imgKey)
+                UserDefaults(suiteName: "group.dicky.iRandomPhotoWidget")!.set(list, forKey: key)
+                UserDefaults(suiteName: "group.dicky.iRandomPhotoWidget")!.synchronize()
+                index += 1
+                if (index == images.count) {
+                    saveAction = true;
+                }
+            }
+            return saveAction
+        }
+
+        func saveImage(_ imageName:String, _ image:UIImage) {
+            if let shareUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.dicky.iRandomPhotoWidget") {
+               let imagePath = shareUrl.appendingPathComponent(imageName)
+                try? image.pngData()?.write(to: imagePath)
+            }
+        }
+        
+        func removeImages() {
+            let list = UserDefaults(suiteName: "group.dicky.iRandomPhotoWidget")!.array(forKey: key) as? [String] ?? [String]()
+            UserDefaults(suiteName: "group.dicky.iRandomPhotoWidget")!.synchronize()
+            for (index, _) in list.enumerated() {
+                let imageName = getImageKey(index)
+                if let shareUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.dicky.iRandomPhotoWidget") {
+                   let imagePath = shareUrl.appendingPathComponent(imageName)
+                    if let image = UIImage(contentsOfFile: imagePath.path) {
+                        do {
+                              if FileManager.default.fileExists(atPath: imagePath.path) {
+                                  try FileManager.default.removeItem(atPath: imagePath.path)
+                               }
+                           } catch let error as NSError {
+                               print(error.debugDescription)
+                           }
+                    }
+                }
+            }
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            
+            parent.picker.toggle()
+            
+            if (results.count > 0) {
+                self.parent.images.removeAll()
+            }
+            
+            for img in results {
+                if img.itemProvider.canLoadObject(ofClass: UIImage.self) {
+                    img.itemProvider.loadObject(ofClass: UIImage.self) { (image,err) in
+                        DispatchQueue.main.async {
+                            guard image != nil else {
+                                return
+                            }
+                            self.parent.images.append(image as! UIImage)
+                            if (img == results.last) {
+                                if self.saveImages(self.parent.images) {
+                                    self.parent.alertBox = true
+                                    WidgetCenter.shared.reloadAllTimelines()
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    
+                }
+            }
+            
+        }
+    }
+}
